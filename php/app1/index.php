69bbebee0f805e4c42a3463be270dd533265207e
Jerson Eduardo Quiñonez <eduardo207@gmail.com>
2025-03-14 19:13:14 -0600

    Cambios de ejemplo de php

---
+++ php/app1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo PHP</title>
+</head>
+<body>
+    <?php
+        // Esto es un comentario en php
+        # Esto es otro comentario en php
+        echo "Hola mundo en PHP";
+    ?>
+</body>
+</html>
